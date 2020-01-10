2b8e0299e339702c6fee3f05060bf59eae0e8177
vickyzou1126 <wanzou1126@gmail.com>
2020-01-10 11:27:53 +1100

    created new components.

--- App.js
+++ App.js
@@ -1,29 +1,21 @@
 import React,{useState} from 'react';
 import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
-  const [enteredItem,setEnteredItem] = useState('');
   const [itemList,setItemList] = useState([]);
 
-  const ItemInputChanged = (enteredItem) => {
-    setEnteredItem(enteredItem)
+  const AddtoList = goalItem =>{
+    setItemList(currentItems => [...currentItems,{key:Math.random().toString(),value:goalItem}])
   }
-  const AddtoList = () =>{
-    setItemList(currentItems => [...currentItems,{key:Math.random().toString(),value:enteredItem}])
-  }
+
   return (
-    <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput placeholder='Add New Item' 
-                  style={styles.input}
-                  onChangeText={ItemInputChanged}
-                  value={enteredItem}/>
-        <Button title='ADD' onPress={AddtoList} />
-      </View>
-      <FlatList data={itemList}
-                renderItem={
-                  itemData => (<View><Text>{itemData.item.value}</Text></View>)
-                }/>
+    <View style = {styles.container}>
+      <GoalInput AddtoList = {AddtoList} />
+      <FlatList data = {itemList}
+                renderItem = {itemData => <GoalItem title = {itemData.item.value} />
+      }/>
     </View>
   );
 }
@@ -32,15 +24,4 @@ const styles = StyleSheet.create({
   screen:{
     padding:50,
   },
-  inputContainer:{
-    flexDirection:'row',
-    justifyContent:'space-between',
-    alignItems:'center'
-  },
-  input:{
-    width:'80%',
-    borderColor:'black',
-    borderWidth:1,
-    padding:10
-  }
 });
